f89d55d0ba78281451dfa3d9294e7b7960be1f85
Mlcnt <millicentllainorte@gmail.com>
2025-06-05 01:51:36 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,7 +3,7 @@ const serviceAccount = require('./firebase-key.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'your-database-url'
+  databaseURL: "https://nosql-crud-a11a9.firebaseio.com" 
 });
 
 const db = admin.firestore();
